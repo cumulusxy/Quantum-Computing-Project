4f59960b0dfe9e3db2b0fe9f81af4901e5377929
cumulusxy <147005965+cumulusxy@users.noreply.github.com>
2023-11-06 18:18:19 -0800

    Initial commit

---
+++ Evolution_Calculation.py
@@ -0,0 +1,42 @@
+import qutip as qt
+import numpy as np
+
+
+class QSystem:
+    def __init__(self, init_state, hamiltonian, time_lis):
+        self.init_state = init_state
+        self.hamiltonian = hamiltonian
+        self.time_lis = time_lis
+        self.state_lis = close_evolve(init_state, hamiltonian, time_lis)
+
+    def slice_coordinate(self):
+        return tuple(self.state_lis[:, i] for i in range(np.shape(self.state_lis)[1]))
+
+class AQCSystem(QSystem):
+    def __init__(self, init_state, hamiltonian_lis, time_schedule, dimless_time_lis, tf):
+        time_lis = dimless_time_lis * tf
+        time_schedule_lis=[lambda x,*args: time_schedule(x,tf,*args)[i] for i in range(len(hamiltonian_lis))]
+        hamiltonian = [[ham, sch] for ham, sch in zip(hamiltonian_lis, time_schedule_lis)]
+        super().__init__(init_state, hamiltonian, time_lis)
+
+
+def close_evolve(initState, hamiltonian, timeLis):
+    objLis = qt.mesolve(hamiltonian, initState, timeLis, [], [])
+    stateLis = [state for state in objLis.states]
+    return np.array(stateLis)
+
+def simple_time_schedule(x, tf, *args):
+    return (tf - x) / tf, x / tf
+
+def optimal_time_schedule(x, tf, n, *args):
+    n=2
+    s = x / tf
+    a = 1 / 2 - 1 / (2 * np.sqrt(n - 1)) * np.tan((2 * s - 1) * np.arctan(np.sqrt(n - 1)))
+    b = 1 / 2 + 1 / (2 * np.sqrt(n - 1)) * np.tan((2 * s - 1) * np.arctan(np.sqrt(n - 1)))
+    return a, b
+
+if __name__ == "__main__":
+    state = qt.Qobj([[1], [0]])
+    hamiltonian = qt.Qobj([[1, 0], [0, -1]])
+    stateLis = close_evolve(state, hamiltonian, np.arange(0, 100, 1))
+    print(stateLis[-1])
---
+++ main.py
@@ -0,0 +1,28 @@
+import qutip as qt
+import numpy as np
+from manim import *
+from Evolution_Calculation import close_evolve, AQCSystem, simple_time_schedule, optimal_time_schedule
+
+
+class RotatingState(Scene):
+    def construct(self):
+        #Plug in the numbers for a0
+        n = 10
+        tf1 = 500
+        dimless_time_lis1 = np.linspace(0, 1, 100)
+        init_state = qt.Qobj([[np.sqrt(1 / n)], [np.sqrt((n - 1) / n)]])
+        hmlta = qt.Qobj([[1-1/n, -np.sqrt(n - 1)/n], [-np.sqrt(n - 1)/n, 1-(n - 1)/n]])
+        hmltb = qt.Qobj([[0,0],[0,1]])
+        simpleGrover=AQCSystem(init_state,[hmlta,hmltb],simple_time_schedule,dimless_time_lis1,tf1)
+
+        #Generate the animation
+        a0=Arrow([0,0,0],[3*np.sqrt(1/n),3*np.sqrt((n - 1)/n),0])
+        x, y = simpleGrover.slice_coordinate()
+        for i in range(1, len(dimless_time_lis1)):
+            self.play(
+                Rotate(
+                    a0,
+                    angle=np.arctan(np.abs(y[i][0]/x[i][0]))-np.arctan(np.abs(y[i-1][0]/x[i-1][0])),
+                    about_point=[0,0,0],
+                    rate_func=linear
+            ))
