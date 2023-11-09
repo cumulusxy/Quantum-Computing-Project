27f615701c6d2ec4b4c48bbd48509ad6c625cd71
cumulusxy <147005965+cumulusxy@users.noreply.github.com>
2023-11-08 16:04:02 -0800

    timer added

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from Evolution_Calculation import close_evolve, AQCSystem, simple_time_schedule,
 class RotatingState(Scene):
     def construct(self):
         #Plug in the numbers for a0
-        n = 10
-        tf1 = 100
-        tf2 = 65
-        dimless_time_lis1,dimless_time_lis2 = generate_dimless_timelis([tf1,tf2],50)
+        n = 15
+        tf0 = 100
+        tf1 = 65
+        dimless_time_lis0,dimless_time_lis1 = generate_dimless_timelis([tf0,tf1],50)
         init_state = qt.Qobj([[np.sqrt(1 / n)], [np.sqrt((n - 1) / n)]])
         hmlta = qt.Qobj([[1-1/n, -np.sqrt(n-1)/n], [-np.sqrt(n-1)/n, 1-(n-1)/n]])
         hmltb = qt.Qobj([[0,0], [0,1]])
 
-        simpleGrover = AQCSystem(init_state,[hmlta,hmltb],simple_time_schedule,dimless_time_lis1,tf1)
-        optimalGrover = AQCSystem(init_state,[hmlta,hmltb],lambda x,tf,i,*args: optimal_time_schedule(x,tf,i,n,*args),dimless_time_lis2,tf2)
+        simpleGrover = AQCSystem(init_state,[hmlta,hmltb],simple_time_schedule,dimless_time_lis0,tf0)
+        optimalGrover = AQCSystem(init_state,[hmlta,hmltb],lambda x,tf,i,*args: optimal_time_schedule(x,tf,i,n,*args),dimless_time_lis1,tf1)
 
 
         #Generate the animation
@@ -27,13 +27,42 @@ class RotatingState(Scene):
 
         x0, y0, x1, y1 = match_by_padding(x0,y0,x1,y1)
 
+        self.len0=len(dimless_time_lis0)
+        self.len1=len(dimless_time_lis1)
+
+        dn0 = DecimalNumber(0)
+        dn0.set_x(-3)
+        dn0.set_y(-2)
+        self.add(dn0)
+        self.clock0 = 0
+        self.now_at = 0
+        dn1 = DecimalNumber(0)
+        dn1.set_x(3)
+        dn1.set_y(-2)
+        self.add(dn1)
+        self.clock1 = 0
+        def update_timer0(mob, dt):
+            if self.now_at<=self.len0:
+                self.clock0 += dt
+                mob.set_value(self.clock0)
+
+
+        def update_timer1(mob, dt):
+            if self.now_at<=self.len1:
+                self.clock1 += dt
+                mob.set_value(self.clock1)
+
+
+        dn0.add_updater(update_timer0)
+        dn1.add_updater(update_timer1)
+
         for i in range(1, len(x1)):
             self.play(
                 Rotate(
                     a0,
                     angle=np.arctan(np.abs(y0[i][0]/x0[i][0]))-np.arctan(np.abs(y0[i-1][0]/x0[i-1][0])),
                     about_point=[-3,0,0],
-                    rate_func=linear
+                    rate_funpic=linear
             ),
             Rotate(
                 a1,
@@ -42,6 +71,7 @@ class RotatingState(Scene):
                 rate_func=linear
             )
             )
+            self.now_at+=1
 
 if __name__=="__main__":
     a=RotatingState()
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import qutip as qt
 import numpy as np
-tmax=700
-time_lis=np.linspace(0,1,300)
-n=3
-init_state = qt.Qobj([[np.sqrt(1 / n)], [np.sqrt((n - 1) / n)]])
-hmlta = qt.Qobj([[1 - 1 / n, -np.sqrt(n - 1) / n], [-np.sqrt(n - 1) / n, 1 - (n - 1) / n]])
-hmltb = qt.Qobj([[0,0],[0,1]])
-print(time_lis*tmax)
-a=qt.mesolve([[hmlta,lambda x,*arg:(tmax-x)/tmax],[hmltb,lambda x,*arg: x/tmax]], init_state, time_lis*tmax, [], [])
-print(np.abs(a.states)[-1])
+from manim import *
+
+class HelloLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r"\LaTeX", font_size=144)
+        self.add(tex)
